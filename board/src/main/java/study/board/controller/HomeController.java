877a5472273e5ac70deb006e50e787c682b81778
choiyuran <133099209+choiyuran@users.noreply.github.com>
2023-12-25 17:47:01 +0900

    board
    board

---
+++ board/src/main/java/study/board/controller/HomeController.java
@@ -0,0 +1,13 @@
+package study.board.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeController {
+    @GetMapping("/home")
+    public String home() {
+        return "home 성공 굿~~";
+    }
+
+}
